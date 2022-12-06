f25211dcd889cfa43dbd5a0bba89c8f80177e7c4
loquy <841243659@qq.com>
2022-12-06 17:40:21 +0800

    add code generator

--- spring-boot-code-generator/src/main/java/com/example/codegenerator/param/ColumnParam.java
+++ spring-boot-code-generator/src/main/java/com/example/codegenerator/param/ColumnParam.java
@@ -1,44 +1,1 @@
-package com.example.codegenerator.constant;
-
-/**
- * @author loquy
- */
-public class DbConstant {
-
-    /**
-     * 数据库时间类型
-     */
-    public static final String[] COLUMNTYPE_TIME = {"datetime", "time", "date", "timestamp"};
-
-    /**
-     * 数据库数字类型
-     */
-    public static final String[] COLUMNTYPE_NUMBER = {"tinyint", "smallint", "mediumint", "int", "number", "integer",
-            "bit", "bigint", "float", "double", "decimal"};
-
-    /**
-     * 字符串类型
-     */
-    public static final String TYPE_STRING = "String";
-
-    /**
-     * 整型
-     */
-    public static final String TYPE_INTEGER = "Integer";
-
-    /**
-     * 长整型
-     */
-    public static final String TYPE_LONG = "Long";
-
-    /**
-     * 高精度计算类型
-     */
-    public static final String TYPE_BIGDECIMAL = "BigDecimal";
-
-
-    /**
-     * 时间类型
-     */
-    public static final String TYPE_DATE = "Date";
-}
+package com.example.codegenerator.param;import lombok.Data;/** * @author loquy */@Datapublic class ColumnParam {    /**     * 数据库字段名称     */    private String columnName;    /**     * 数据库字段类型     */    private String columnType;    /**     * 数据库字段首字母小写且去掉下划线字符串     */    private String changeColumnName;    /**     * 数据库字段注释     */    private String columnComment;    /**     * 主键     */    private String primaryKey;    /**     * java 类型     */    private String javaType;}
