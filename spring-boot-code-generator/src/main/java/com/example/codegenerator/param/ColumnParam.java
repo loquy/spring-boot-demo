2a992569a8003f4470214f75c472be2542b69ab2
loquy <47681271+loquy@users.noreply.github.com>
2022-12-07 17:00:59 +0800

    update
    update

--- spring-boot-code-generator/src/main/java/com/example/codegenerator/param/ColumnParam.java
+++ spring-boot-code-generator/src/main/java/com/example/codegenerator/param/ColumnParam.java
@@ -1,1 +1,40 @@
-package com.example.codegenerator.param;import lombok.Data;/** * @author loquy */@Datapublic class ColumnParam {    /**     * 数据库字段名称     */    private String columnName;    /**     * 数据库字段类型     */    private String columnType;    /**     * 数据库字段首字母小写且去掉下划线字符串     */    private String changeColumnName;    /**     * 数据库字段注释     */    private String columnComment;    /**     * 主键     */    private String primaryKey;    /**     * java 类型     */    private String javaType;}
+package com.example.codegenerator.param;
+
+import lombok.Data;
+
+/**
+ * @author loquy
+ */
+@Data
+public class ColumnParam {
+
+    /**
+     * 数据库字段名称
+     */
+    private String columnName;
+
+    /**
+     * 数据库字段类型
+     */
+    private String columnType;
+
+    /**
+     * 数据库字段首字母小写且去掉下划线字符串
+     */
+    private String changeColumnName;
+
+    /**
+     * 数据库字段注释
+     */
+    private String columnComment;
+
+    /**
+     * 主键
+     */
+    private String primaryKey;
+
+    /**
+     * java 类型
+     */
+    private String javaType;
+}
